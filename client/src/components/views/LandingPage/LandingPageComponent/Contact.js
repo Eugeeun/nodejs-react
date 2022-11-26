b70f0f7efe2d933de33416873815f25bd15f6e44
Eugeeun <97161495+Eugeeun@users.noreply.github.com>
2022-11-27 02:37:25 +0900

    add myinfo.json

--- client/src/components/views/LandingPage/LandingPageComponent/Contact.js
+++ client/src/components/views/LandingPage/LandingPageComponent/Contact.js
@@ -4,38 +4,27 @@ import {
   faGithubSquare,
   faInstagram,
 } from '@fortawesome/free-brands-svg-icons';
+import myData from '../data/myinfo.json';
 
 function Contact() {
+  const links = [faGithubSquare, faInstagram];
+
   return (
     <section id="contact" className="section">
       <h1 className="contact__title">Contact Me</h1>
       <ul className="contact__infos">
-        <li>
-          <h2 className="contact__email">jack603@naver.com</h2>
-        </li>
-        <li>
-          <h2 className="contact__email">gojongwon8292@gmail.com</h2>
-        </li>
-        <li>
-          <h2 className="contact__tel">010-9447-1363</h2>
-        </li>
+        {myData.contacts.map((contact, index) => (
+          <li key={index}>
+            <h2 className={contact.className}>{contact.content}</h2>
+          </li>
+        ))}
       </ul>
       <div className="contact__links">
-        <a href="https://github.com/Eugeeun" target="blank">
-          <FontAwesomeIcon
-            icon={faGithubSquare}
-            className="fab fa-github-square contact__link"
-          />
-        </a>
-        <a
-          href="https://www.instagram.com/colored_pencils99.11.22/"
-          target="blank"
-        >
-          <FontAwesomeIcon
-            icon={faInstagram}
-            className="fa-brands fa-instagram contact__link"
-          />
-        </a>
+        {myData.links.map((link, index) => (
+          <a href={link.link} target="blank">
+            <FontAwesomeIcon icon={links[index]} className={link.className} />
+          </a>
+        ))}
       </div>
       <p className="contact__rights">
         Copyright 2022. JongWon. All rights reserved.
